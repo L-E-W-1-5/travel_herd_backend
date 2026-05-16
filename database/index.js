fb8bf4b4d08197c4d8a5ea07df71028851fcbc76
Lewis Wootton <111094589+L-E-W-1-5@users.noreply.github.com>
2026-05-16 22:05:16 +0100

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,5 +1,16 @@
 import * as pg from 'pg'
 const { Pool } = pg.default
+
+
+import { neon } from "@neondatabase/serverless";
+
+
+
+
+export const neonConnection = neon(process.env.DATABASE_URL);
+
+const databaseUrl = process.env.POSTGRES_CONNECTION_URL;
+
 const pool = new Pool({
   connectionString: process.env.POSTGRES_CONNECTION_URL,
 });
