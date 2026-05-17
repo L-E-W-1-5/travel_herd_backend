54dd5d142360025bcec61098da216d096f0ab841
Lewis Wootton <111094589+L-E-W-1-5@users.noreply.github.com>
2026-05-17 03:01:25 +0100

    Update users.js

--- models/users.js
+++ models/users.js
@@ -25,7 +25,7 @@ export async function getUsers(id, body) {
         `SELECT * FROM users WHERE users.auth_id = '${id}'` 
     )
 
-    if (!userReturn? || userReturn?.length === 0){
+    if (!userReturn? | userReturn?.length === 0){
         userReturn = await neonConnection.query(
             `INSERT INTO users (auth_id, name, email) VALUES ('${body.sub}', '${body.name}', '${body.email}') RETURNING *;` 
         )  
